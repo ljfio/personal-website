891af52083ae939b2d143747f3725a3491a4b3b0
Luke Fisher <mail@lukefisher.me>
2020-11-01 15:24:50 +0000

    Add in navbar

---
+++ components/MainNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="">
+    <div class="container mx-auto flex flex-row">
+      <div class="flex-grow">
+        <div class="p-4 inline-block">
+          <nuxt-link class="text-white hover:text-brand font-light" to="/"
+            >Home</nuxt-link
+          >
+        </div>
+        <div class="p-4 inline-block">
+          <nuxt-link class="text-white hover:text-brand font-light" to="/blog"
+            >Blog</nuxt-link
+          >
+        </div>
+        <div class="p-4 inline-block">
+          <nuxt-link class="text-white hover:text-brand font-light" to="/projects"
+            >Projects</nuxt-link
+          >
+        </div>
+      </div>
+      <div class="">
+        <div class="p-4 inline-block">
+          <nuxt-link class="text-white hover:text-brand font-light" to="/contact">
+            Contact
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
