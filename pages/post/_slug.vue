8a1f8a6bc2569df6ff460dccf0af4e44ddf6e929
Luke Fisher <mail@lukefisher.me>
2020-11-08 19:10:16 +0000

    Add in pager to post view

--- pages/post/_slug.vue
+++ pages/post/_slug.vue
@@ -1,14 +1,27 @@
 <template>
   <article class="">
     <div class="bg-dark-two text-white">
-      <div class="container mx-auto py-10">
+      <div class="container mx-auto py-10 px-4">
         <h1 class="text-5xl">{{ page.title }}</h1>
         <h2 class="text-2xl">{{ page.description }}</h2>
         <h3 class="mt-2">{{ $moment(page.publishedAt).format("LLL") }}</h3>
       </div>
     </div>
-    <div class="container mx-auto py-2">
+    <div class="container mx-auto py-4 px-4">
       <nuxt-content :document="page" />
+
+      <div class="mt-10 mb-6 flex flex-row">
+        <div class="flex-grow">
+          <nuxt-link :to="{ name: 'post-slug', params: { slug: prev.slug } }" class="text-dark-two hover:text-brand" v-if="prev">
+            &larr; {{prev.title}}
+          </nuxt-link>
+        </div>
+        <div class="">
+          <nuxt-link :to="{ name: 'post-slug', params: { slug: next.slug } }" class="text-dark-two hover:text-brand" v-if="next">
+            {{next.title}} &rarr;
+          </nuxt-link>
+        </div>
+      </div>
     </div>
   </article>
 </template>
@@ -25,8 +38,17 @@ export default {
       .where({ published: true })
       .fetch();
 
+    const [prev, next] = await $content("articles")
+      .where({ published: true })
+      .sortBy('publishedAt', 'desc')
+      .only(['title', 'slug'])
+      .surround(page.slug)
+      .fetch();
+
     return {
       page,
+      prev,
+      next,
     };
   },
 };
