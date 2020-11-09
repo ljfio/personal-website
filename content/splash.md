c7be69f4b07cdb20ee88712fc7ef4e5b662759e6
Luke Fisher <mail@lukefisher.me>
2020-11-09 19:36:08 +0000

    Move splash text to markdown

---
+++ content/splash.md
@@ -0,0 +1,19 @@
+---
+title: Luke Fisher
+description: Applications Developer
+---
+
+Forward thinking software engineer and designer.
+
+Working in an agile team of problem solvers, supporting bespoke .NET software solutions.
+
+Graduated from The Robert Gordon University (RGU) in Aberdeen with a First Class BSc (Hons) in Computer Science.
+
+Elected President of the [RGU Computing Society][1] for the 2016-17 and 2017-18 terms.
+
+Strong advocate for digital privacy and data security. Member of both the [EFF][2] and [ORG][3],
+two organisations standing up to protect your digital rights.
+
+[1]: https://rgucs.com
+[2]: https://eff.org
+[3]: https://openrightsgroup.org
--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,9 @@
     </div>
     <div class="profile">
       <div class="container mx-auto px-4 py-6">
-        <div class="flex flex-col space-y-6 sm:space-y-0 sm:flex-row sm:space-x-6">
+        <div
+          class="flex flex-col space-y-6 sm:space-y-0 sm:flex-row sm:space-x-6"
+        >
           <div class="xs:w-1/4">
             <img
               src="//s.gravatar.com/avatar/8bf98d1d6ea81a5e467b030a5a0b4798?s=600"
@@ -20,48 +22,33 @@
             />
           </div>
           <div class="xs:w-3/4">
-            <h1 class="text-4xl leading-none mb-2">Luke Fisher</h1>
-            <h2 class="text-2xl leading-none mb-2">Applications Developer</h2>
-            <p>Forward thinking Software Engineer and Designer.</p>
-            <p>
-              Working in an agile team of problem solvers, supporting bespoke
-              .NET software solutions.
-            </p>
-            <p>
-              Graduated from The Robert Gordon University (RGU) in Aberdeen with
-              a First Class BSc (Hons) in Computer Science.
-            </p>
-            <p>
-              Elected President of the
-              <a target="_blank" href="//rgucs.com">RGU Computing Society</a>
-              for the 2016-17 and 2017-18 terms. Since 2015 I have been a
-              Student member of the
-              <a target="_blank" href="//ieee.org">IEEE</a> and
-              <a target="_blank" href="//computer.org">IEEE Computer Society</a
-              >.
-            </p>
-            <p>
-              Strong advocate for digital privacy and data security. Member of
-              both the <a target="_blank" href="//eff.org">EFF</a> and
-              <a target="_blank" href="//openrightsgroup.org">ORG</a>, two
-              organisations standing up to protect your digital rights.
-            </p>
+            <h1 class="text-5xl leading-none mb-2">{{ content.title }}</h1>
+            <h2 class="text-2xl leading-none mb-2">
+              {{ content.description }}
+            </h2>
+            <nuxt-content :document="content"></nuxt-content>
           </div>
         </div>
       </div>
     </div>
     <div class="attainment bg-black text-white">
       <div class="container mx-auto px-4 py-4">
-        <div class="flex flex-col space-y-6 sm:space-y-0 sm:flex-row sm:space-x-6">
+        <div
+          class="flex flex-col space-y-6 sm:space-y-0 sm:flex-row sm:space-x-6"
+        >
           <div class="sm:w-1/2">
             <h2 class="text-2xl text-center">Experience</h2>
             <div class="py-2 font-light text-xl leading-none">
               Applications Developer at Hydrasun
-              <div class="pt-2 text-sm text-gray-500">August 2018 - Present</div>
+              <div class="pt-2 text-sm text-gray-500">
+                August 2018 - Present
+              </div>
             </div>
             <div class="py-2 font-light text-xl leading-none">
               Software Engineer at Codify
-              <div class="pt-2 text-sm text-gray-500">June 2015 - August 2018</div>
+              <div class="pt-2 text-sm text-gray-500">
+                June 2015 - August 2018
+              </div>
             </div>
             <div class="py-2 font-light text-xl leading-none">
               Software Developer (Temporary) at University of Aberdeen
@@ -69,18 +56,24 @@
             </div>
             <div class="py-2 font-light text-xl leading-none">
               Developer (Student Placement) at Hydrasun
-              <div class="pt-2 text-sm text-gray-500">July 2014 - August 2014</div>
+              <div class="pt-2 text-sm text-gray-500">
+                July 2014 - August 2014
+              </div>
             </div>
           </div>
           <div class="sm:w-1/2">
             <h2 class="text-2xl text-center">Achievements</h2>
             <div class="py-2 font-light text-xl leading-none">
               First Class BSc (Hons) in Computer Science
-              <div class="pt-2 text-sm text-gray-500">Completed first, second and degree years with Distinction</div>
+              <div class="pt-2 text-sm text-gray-500">
+                Completed first, second and degree years with Distinction
+              </div>
             </div>
             <div class="py-2 font-light text-xl leading-none">
               ConocoPhillips Award for Academic Excellence
-              <div class="pt-2 text-sm text-gray-500">Attained at RGU for both Year 1 &amp; 2</div>
+              <div class="pt-2 text-sm text-gray-500">
+                Attained at RGU for both Year 1 &amp; 2
+              </div>
             </div>
             <div class="py-2 font-light text-xl leading-none">
               Award for Innovation at STEM in the Pipeline
@@ -104,6 +97,13 @@ export default {
       description: "Luke Fisher's personal website",
     };
   },
+  async asyncData({ $content }) {
+    const content = await $content("splash").fetch();
+
+    return {
+      content,
+    };
+  },
 };
 </script>
 
@@ -118,22 +118,20 @@ export default {
 
 .grayscale {
   -webkit-filter: grayscale(100%);
-    filter: grayscale(100%);
+  filter: grayscale(100%);
 }
 
-p {
-  @apply font-light text-xl leading-tight py-1;
-}
+::v-deep .nuxt-content {
+  & p {
+    @apply font-light text-xl leading-tight py-1;
+  }
 
-.attainment {
+  & a {
+    @apply text-dark-two;
 
-}
-
-a {
-  @apply text-dark-two;
-
-  &:hover {
-    @apply text-brand;
+    &:hover {
+      @apply text-brand;
+    }
   }
 }
 </style>
