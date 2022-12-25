4b30d1ed6df4cb522374fbc786a2583a46ebeeb3
Luke Fisher <mail@lukefisher.me>
2022-12-25 13:42:00 +0000

    Move resume experience to content collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,23 @@
 import { z, defineCollection } from "astro:content";
 
 const articles = defineCollection({
-    schema: {
-        title: z.string(),
-        description: z.string(),
-        published: z.date(),
-    }
+  schema: {
+    title: z.string(),
+    description: z.string(),
+    published: z.date(),
+  }
+});
+
+const experience = defineCollection({
+  schema: {
+    title: z.string(),
+    company: z.string(),
+    start: z.string(),
+    finish: z.string().nullable()
+  }
 });
 
 export const collections = {
-    articles,
+  articles,
+  experience
 };
