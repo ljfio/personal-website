784f29c3079c10b185943bb452405d5c21a8645d
Luke Fisher <mail@lukefisher.me>
2022-12-25 04:19:53 +0000

    Use import.meta.env instead of process.env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly COMMIT_SHA: string;
+    readonly GITHUB_SERVER_URL: string;
+    readonly GITHUB_REPOSITORY: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
