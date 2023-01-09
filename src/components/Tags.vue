343a93dece19febcd084b3bbcd50a76b76ce9471
Luke Fisher <mail@lukefisher.me>
2023-01-09 23:15:02 +0000

    Fix allow no tags to be set

--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex flex-row gap-x-2 font-light text-sm">
+  <div class="flex flex-row gap-x-2 font-light text-sm"
+       v-if="tags">
     <a class="px-2 py-1 bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 hover:dark:bg-gray-700 rounded"
        v-for="tag in tags"
        :key="tag"
@@ -11,7 +12,7 @@
 
 <script lang="ts" setup>
 interface TagsProps {
-  tags: string[];
+  tags?: string[];
 };
 
 defineProps<TagsProps>();
