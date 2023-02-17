4effcd013e7087fa7d4c6d33a5aece521e179bef
Luke Fisher <mail@lukefisher.me>
2023-02-17 04:17:48 +0000

    Add client-side TimeSince component to display correct date

---
+++ src/components/TimeSince.vue
@@ -0,0 +1,20 @@
+<template>
+    <span :title="formatted">{{since}}</span>
+</template>
+
+<script lang="ts" setup>
+import moment from 'moment';
+
+interface TimeSinceProps {
+    date: Date;
+}
+
+const props = defineProps<TimeSinceProps>();
+
+const { date } = props;
+
+const value = moment(date);
+
+const formatted = value.format("LLLL");
+const since = value.fromNow();
+</script>
