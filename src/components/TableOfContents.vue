26763c734eeee2c8d1f930c0edd0eaa8ec222df3
Luke Fisher <mail@lukefisher.me>
2023-02-18 23:23:27 +0000

    Add in TOC to articles #3

---
+++ src/components/TableOfContents.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="bg-gray-200 dark:bg-gray-800 h-fit min-w-fit lg:sticky top-4 flex flex-col">
+    <a class="px-4 py-2 block hover:bg-gray-300 hover:dark:bg-gray-700"
+       v-for="heading in headings"
+       :key="heading.slug"
+       :href="`#${heading.slug}`">
+      {{ heading.text }}
+    </a>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import type { MarkdownHeading } from 'astro';
+
+interface Props {
+  headings: MarkdownHeading[];
+}
+
+const props = defineProps<Props>();
+
+const { headings } = props;
+</script>
