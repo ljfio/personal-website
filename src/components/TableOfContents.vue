94329502c7c4a978db743ff0f2bddf5bf6be487c
Luke Fisher <mail@lukefisher.me>
2023-02-19 00:23:18 +0000

    Update table of contents style

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-gray-200 dark:bg-gray-800 h-fit min-w-fit lg:sticky top-4 flex flex-col">
+  <nav class="bg-gray-200 dark:bg-gray-800 h-fit min-w-fit rounded lg:sticky top-4 flex flex-col mb-8">
     <a class="px-4 py-2 block hover:bg-gray-300 hover:dark:bg-gray-700"
        v-for="heading in headings"
        :key="heading.slug"
