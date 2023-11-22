e9ba7a89cc3e2b7c97e491736fde9acf07b9f97e
Luke Fisher <mail@lukefisher.me>
2023-11-22 07:29:37 +0000

    Tweak menu to be full screen

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -9,12 +9,28 @@
       </a>
     </div>
 
-    <div class="menu-dropdown"
+    <div class="menu-container"
          :class="dropdownClass">
-      <div class="menu-links">
-        <a :href="link.url"
-           v-for="link in links"
-           :key="link.url">{{ link.label }}</a>
+
+      <div class="menu-overlay">
+        <div class="menu-background"
+             v-on:click="toggleMenu">
+        </div>
+
+        <div class="menu-dropdown">
+          <div class="menu-links">
+            <a :href="link.url"
+               v-for="link in links"
+               :key="link.url">{{ link.label }}</a>
+          </div>
+
+          <div class="menu-dropdown-close">
+            <a href="#"
+               v-on:click="toggleMenu">
+              &Cross; Exit
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
@@ -62,15 +78,31 @@ nav.menu {
     }
   }
 
+  .menu-container {
+    @apply fixed right-0 top-0 bottom-0 left-0 w-full h-full z-50;
+  }
+
+  .menu-overlay {
+    @apply flex flex-row w-full h-full;
+
+    .menu-background {
+      @apply flex-grow bg-gray-400 dark:bg-gray-600 opacity-80 cursor-pointer;
+    }
+  }
+
   .menu-dropdown {
-    @apply absolute right-0 mt-4 rounded-lg bg-gray-200 dark:bg-gray-800;
+    @apply bg-gray-200 dark:bg-gray-800 text-4xl w-full md:w-96 px-8 flex flex-col;
 
     .menu-links {
-      @apply flex flex-col py-2;
+      @apply flex flex-col w-full gap-6 mt-16 flex-grow;
 
       a {
-        @apply block py-2 px-4 underline hover:no-underline;
+        @apply block underline hover:no-underline;
       }
+    }
+
+    .menu-dropdown-close {
+      @apply my-8;
     }
   }
 }
