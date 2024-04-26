915508c50f65dc4459edeb5a48b8799830b5d2be
Luke Fisher <mail@lukefisher.me>
2024-04-26 07:42:34 +0100

    Move links to footer

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,10 +2,10 @@
 /// <reference path="../.astro/types.d.ts" />
 
 interface ImportMetaEnv {
-    readonly GITHUB_SHA: string;
-    readonly CF_PAGES_COMMIT_SHA: string;
-    readonly GITHUB_SERVER_URL: string;
-    readonly GITHUB_REPOSITORY: string;
+    readonly GITHUB_SHA: string?;
+    readonly CF_PAGES_COMMIT_SHA: string?;
+    readonly GITHUB_SERVER_URL: string?;
+    readonly GITHUB_REPOSITORY: string?;
 }
 
 interface ImportMeta {
