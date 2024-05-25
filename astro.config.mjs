5ab39ea217d8b9042758ec6f10aa6501ee147319
Luke Fisher <mail@lukefisher.me>
2024-05-25 09:36:12 +0100

    Cleanup config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import vue from "@astrojs/vue";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -27,6 +22,6 @@ export default defineConfig({
       lastmod: new Date(),
     }),
     vue(),
-    tailwind()
+    tailwind(),
   ]
 });
